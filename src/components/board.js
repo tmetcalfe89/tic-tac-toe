9a4481780c1a33312797d68caaaeec67a0e65078
Tim <tmetcalfe89@gmail.com>
2021-07-07 18:17:54 -0700

    Added options on ToastContainer explicitly

--- src/components/board.js
+++ src/components/board.js
@@ -100,7 +100,17 @@ export default function Board() {
           ))}
         </div>
       ))}
-      <ToastContainer />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
